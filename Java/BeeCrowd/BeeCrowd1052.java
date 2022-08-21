f64eedd2554c5913bc6623bfecc4699b325ccb7b
Matheus Maia <92889028+mathemaia@users.noreply.github.com>
2022-08-21 11:59:53 -0300

    Update BeeCrowd1052.java

--- Java/BeeCrowd/BeeCrowd1052.java
+++ Java/BeeCrowd/BeeCrowd1052.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 /**
  * Leia um valor inteiro entre 1 e 12, inclusive. Correspondente a este valor, deve ser apresentado como resposta o mês do ano por extenso, 
  * em inglês, com a primeira letra maiúscula.
+ *      e.x: 4 -> April
  *
  * @author Matheus Maia
  * @version 20/08/2022
