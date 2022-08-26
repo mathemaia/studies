1817fcc773350d1206128cb479dfc61b5996cdcc
mathemaia <matheusmaiadasilva@gmail.com>
2022-08-26 15:06:13 -0300

    update the code and algoritmo

--- Python/GrupoDeAlgoritmos/intervalos/intervalos.py
+++ Python/GrupoDeAlgoritmos/intervalos/intervalos.py
@@ -1,5 +1,5 @@
 # leitura do arquivo e instância de variaveis
-entradas = open('intervalos.txt').read().splitlines()
+entradas = open('teste.txt').read().splitlines()
 inter = [0] * len(entradas)
 comeco_intervalo = [0] * len(entradas)
 fim_intervalo = [0] * len(entradas)
@@ -13,17 +13,28 @@ for i, elementos in enumerate(inter):
     comeco_intervalo[i] = int(elementos.split('-')[0])
     fim_intervalo[i] = int(elementos.split('-')[1])
 
-# conferindo se o numero posterior ao intervalo atual está no seguinte
-for i in range(len(entradas)):
-    if i + 1 < len(entradas):
-        if (comeco_intervalo[i + 1]) <= (fim_intervalo[i] + 1) <= (fim_intervalo[i + 1]):
-            continue
-        else: 
-            fora_dos_intervalos.append(fim_intervalo[i] + 1)
+# identifica o intervalo cujo primeiro número é o menor
+menor = comeco_intervalo[0]
+indice = 0
+for i in range(1, len(comeco_intervalo)):
+    if menor > comeco_intervalo[i]:
+        menor = comeco_intervalo[i]
+        indice = i + 1
 
-# encontrando o menor
-menor = fora_dos_intervalos[0]
-for i in range(1, len(fora_dos_intervalos)):
-    pass
+# checa se o útimo número e o posterior pertencem a um intervalo
+count = 0
+numero = menor
+while True:
+    if (indice != count) and (comeco_intervalo[count] <= numero <= fim_intervalo[count]):
+        numero = fim_intervalo[count]
+        indice = count
+    else:
+        for idx, i in enumerate(fim_intervalo):
+            if comeco_intervalo[idx] <= (numero + 1) <= fim_intervalo[idx]:
+                break
+            else:
+                fora_dos_intervalos.append(numero + 1)
+    count += 1
+        
 
-print(len(fora_dos_intervalos))
+
