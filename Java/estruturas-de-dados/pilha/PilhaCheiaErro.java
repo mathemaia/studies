6a2e4ad10a274d55280f8c4b716f2d081625d12f
mathemaia <matheusmaiadasilva@gmail.com>
2022-08-28 22:18:06 -0300

    added pilha dir

---
+++ Java/estruturas-de-dados/pilha/PilhaCheiaErro.java
@@ -0,0 +1,10 @@
+package pilha;
+
+/**
+ * Classe que faz o tratamento de erro para pilha cheia
+ * @author matheus maia
+ */
+
+public class PilhaCheiaErro extends RuntimeException {
+    public PilhaCheiaErro(String mensagem) { super(mensagem); }
+}
