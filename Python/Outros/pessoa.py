6aca5652d54cf68f8ff5d20b6993ee47aea91362
mathemaia <matheusmaiadasilva@gmail.com>
2022-09-05 15:38:44 -0300

    pessoa.py update

--- Python/Outros/pessoa.py
+++ Python/Outros/pessoa.py
@@ -3,13 +3,15 @@ from datetime import datetime
 class Pessoa:
     ano_atual = int(datetime.strftime(datetime.now(), '%Y'))
 
+
     # Construtor
     def __init__(self, nome, idade, comendo = False, falando = False):
         self.nome = nome
         self.idade = idade
         self.comendo = comendo
         self.falando = falando
-    
+
+
     # Método para começar a falar
     def falar(self, assunto):
         if self.comendo:
@@ -23,6 +25,7 @@ class Pessoa:
         print(f'{self.nome} está falando sobre {assunto}!')
         self.falando = True
 
+
     # Método para parar de falar
     def parar_de_falar(self):
         if not self.falando:
@@ -31,6 +34,7 @@ class Pessoa:
         
         print(f'{self.nome} parou de falar.')
         self.falando = False
+
 
     # Método para começar a comer
     def comer(self, comida):
@@ -45,6 +49,7 @@ class Pessoa:
         print(f'{self.nome} está comendo {comida}!')
         self.comendo = True
 
+
     # Método para parar de comer
     def parar_de_comer(self):
         if not self.comendo:
@@ -53,7 +58,21 @@ class Pessoa:
 
         print(f'Ok, {self.nome} parou de comer.')
         self.comendo = False
-    
+
+
     # Método que retorna o ano de nascimento
     def get_ano_nascimento(self):
         return self.ano_atual - self.idade
+
+
+    # Método de classe para calcular o ano de nascimento
+    @classmethod
+    def por_ano_nascimento(cls, nome, ano_nascimendo):
+        idade = cls.ano_atual - ano_nascimendo
+        return cls(nome, idade)
+    
+
+## MAIN
+if __name__ == '__main__':
+    p1 = Pessoa()
+
