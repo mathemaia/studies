8e9992c6133803f0caaac15f0d2cefdacee71a4a
mathemaia <matheusmaiadasilva@gmail.com>
2022-10-09 18:38:32 -0300

    ../Assembly-MIPS update

--- Assembly-MIPS/multiplicacao.asm
+++ Assembly-MIPS/multiplicacao.asm
@@ -1,11 +1,12 @@
+.data
+A: .word 12
+B: .word 7
+C: .word 0
+
 .text
 .globl main
-main:	li $t1, 2		# Carrega imediatamente 2 no reg. $t1
-	addi $t2, $zero, 3	# Adiciona 0+3 e carrega para o reg. $t2
-	mul $s0, $t1, $t2	# Multiplica ($t1 * $t2) e guara em $s0
-	move $a0, $s0		# Move o valor de $s0 para $a0
-	li $v0, 1		# Código para impressão de inteiros
-	syscall
+main:	la $t0, a	# Carrega o endereço de A para $t0 
+	lw $t1, 0($t0)	# Carrega o conteúdo de A para $t1
+	la $t2, b	
+	lw $t3, 4($t0)
 	
-	li $v0, 10		# Código que finaliza o programa
-	syscall
