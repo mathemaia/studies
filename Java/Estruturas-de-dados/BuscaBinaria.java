8b4274d75cad67f7bc934a7e067754a30c214b93
mathemaia <matheusmaiadasilva@gmail.com>
2022-11-21 23:57:13 -0300

    update

---
+++ Java/Estruturas-de-dados/BuscaBinaria.java
@@ -0,0 +1,43 @@
+/**
+ * Classe com um método de busca binária.
+ * @author matheus maia
+ * @version 21-09-2022
+ */
+
+public class BuscaBinaria {
+    /**
+     * Método que faz a busca de forma recursiva.
+     * @param numero numero a ser procurado.
+     * @param vetor vetor de números (deve estar organizado).
+     * @param low referência do menor.
+     * @param high referência do maior
+     * @return índice do número.
+     */
+    public static int buscaBinaria(int numero, int[] vetor, int low, int high) {
+        int mid = (low + high) / 2;
+
+        // Se encontrar.
+        if (numero == vetor[mid]) {
+            return mid;
+        }
+
+        // Se não encontrar, retorna -1.
+        if (low == high) {
+            return -1;
+        }
+
+        // Faz a busca recursiva.
+        if (numero > vetor[mid]) {
+            return buscaBinaria(numero, vetor, mid + 1, high);
+        } else {
+            return buscaBinaria(numero, vetor, low, mid -1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] vetor = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        System.out.println(buscaBinaria(2, vetor, 0, vetor.length));
+    }
+}
+
+
