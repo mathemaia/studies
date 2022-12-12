17987e12a5e3c4fe310dc12051b4d03dc442af5c
mathemaia <matheusmaiadasilva@gmail.com>
2022-12-12 11:04:48 -0300

    update

---
+++ Python/Estruturas-de-dados/Arvores/BinaryTree.py
@@ -0,0 +1,8 @@
+class Node:
+        def __init__(self, element: int, left: 'Node', right: 'Node') -> None:
+            self._element: int = element
+            self._left: 'Node' = left
+            self._right: 'Node' = right
+
+class BinaryTree:
+    pass
