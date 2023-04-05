52daddf2011caa65c1559a8e31c51d1cf4f929d5
mathemaia <matheusmaiadasilva@gmail.com>
2023-04-04 21:22:44 -0300

    update

--- Java/Estruturas-de-Dados/Trabalhos/Problema-dos-Macaquinhos/Macaquinho.java
+++ Java/Estruturas-de-Dados/Trabalhos/Problema-dos-Macaquinhos/Macaquinho.java
@@ -1,17 +1,13 @@
 package Trabalho;
 
 
-import java.util.Arrays;
-import java.io.IOException;
-
-
 public class Macaquinho {
     // Numero de identificação
     private int numero;
     // Para qual macaquinho enviar os coquinhos com pedrinhas PARES
-    private int par;
+    private int enviarPar;
     // Para qual macaquinho enviar os coquinhos com pedrinhas IMPARES
-    private int impar;
+    private int enviarImpar;
     // Quantidade total de coquinhos (PARES + IMPARES)
     private int coquinhosTotal;
     // Vetor com todos os coquinhos pares (quantidade de pedrinhas)
@@ -19,27 +15,23 @@ public class Macaquinho {
     // Vetor com todos os coquinhos impares (quantidade de pedrinhas)
     private int[] coquinhosImpares;
 
-
     /**
      * Construtor
      * @param numero Numero de identificação
-     * @param par Para onde enviar os coquinhos com quantidade par de pedrinhas
-     * @param impar Para onde enviar os coquinhos com quantidade impar de pedrinhas
+     * @param enviarPar Para onde enviar os coquinhos com quantidade par de pedrinhas
+     * @param enviarImpar Para onde enviar os coquinhos com quantidade impar de pedrinhas
      * @param coquinhosTotal Quantidade total de coquinhos
      */
-    public Macaquinho(int numero, int par, int impar, int coquinhosTotal, int[] coquinhosPares, int[] coquinhosImpares) {
+    public Macaquinho(int numero, int enviarPar, int enviarImpar, int coquinhosTotal, int[] coquinhosPares, int[] coquinhosImpares) {
         this.numero = numero;
-        this.par = par;
-        this.impar = impar;
+        this.enviarPar = enviarPar;
+        this.enviarImpar = enviarImpar;
         this.coquinhosTotal = coquinhosTotal;
         this.coquinhosPares = coquinhosPares;
         this.coquinhosImpares = coquinhosImpares;
     }
 
-
-    /**
-     * Setters
-     */
+    // Setters
     /**
      * Define um novo valor/novo vetor de coquinhos com quanidade PAR de pedrinhas ao unir o novo vetor com o antigo
      * @param coquinhosPares vetor que irá ser unido com o atual
@@ -55,8 +47,6 @@ public class Macaquinho {
         this.coquinhosPares = aux;
         // Modifica o valor total de coquinhos do macaquinho atual
         this.coquinhosTotal = this.coquinhosPares.length + this.coquinhosImpares.length;
-
-        System.out.println(this.numero + "   pares: " + Arrays.toString(this.coquinhosPares));
     }
     /**
      * Define um novo valor/novo vetor de coquinhos com quanidade IMPAR de pedrinhas ao unir o novo vetor com o antigo
@@ -73,27 +63,25 @@ public class Macaquinho {
         this.coquinhosImpares = aux;
         // Modifica o valor total de coquinhos do macaquinho atual
         this.coquinhosTotal = this.coquinhosPares.length + this.coquinhosImpares.length;
-
-        System.out.println(this.numero + "   impares: " + Arrays.toString(this.coquinhosImpares));
     }
 
-
-    /**
-     * Getters
-     */
+    // Getters
+    public int getNumero() {
+        return numero;
+    }
     /**
      * Devolve o número do macaquinho que irá receber os coquinhos com quantidade PAR
      * @return nº do macaquinho
      */
-    public int getPar() {
-        return this.par;
+    public int getEnviarPar() {
+        return this.enviarPar;
     }
     /**
      * Devolve o nº do macaquinho que irá receber os coquinhos com quantidade IMPAR
      * @return nº do macaquinho
      */
-    public int getImpar() {
-        return this.impar;
+    public int getEnviarImpar() {
+        return this.enviarImpar;
     }
     /**
      * Devolve um vetor com os coquinhos de pedrinhas com quantidades PARES o zera
--- Java/Estruturas-de-Dados/Trabalhos/Problema-dos-Macaquinhos/Main.java
+++ Java/Estruturas-de-Dados/Trabalhos/Problema-dos-Macaquinhos/Main.java
@@ -3,14 +3,14 @@ package Trabalho;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.*;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(new File("src/Trabalho/infos/casos/caso0010.txt"));
+        Scanner in = new Scanner(new File("src/Trabalho/infos/casos/caso0050.txt"));
         ListaDuplamenteEncadeada listaDeMacaquinhos = new ListaDuplamenteEncadeada();
         int idx = 0;
+        int rodadas = 0;
 
         // Separa as infos de cada linha e instancia os objetos Macaquinhos
         while (in.hasNextLine()) {
@@ -20,10 +20,10 @@ public class Main {
 
                 // Numero de identificação do Macaquinho
                 int numero = Integer.parseInt(linha[0].split(" ")[1]);
-                // Para onde enviar os coquinhos com quantidade par de pedrinhas
-                int par = Integer.parseInt(linha[0].split(" ")[4]);
-                // Para onde enviar os coquinhos com quantidade impar de pedrinhas
-                int impar = Integer.parseInt(linha[0].split(" ")[7]);
+                // Para onde enviar os coquinhos com quantidade PAR de pedrinhas
+                int enviarPar = Integer.parseInt(linha[0].split(" ")[4]);
+                // Para onde enviar os coquinhos com quantidade IMPAR de pedrinhas
+                int enviarImpar = Integer.parseInt(linha[0].split(" ")[7]);
                 // Quantidade total de coquinhos
                 int coquinhosTotal = Integer.parseInt(linha[1].strip());
                 // Separa o vetor de coquinhos em um vetor de coquinhos com numero par e impar de pedrinhas
@@ -32,57 +32,55 @@ public class Main {
                 int[] coquinhosImpares = Arrays.stream(aux).filter(n -> n % 2 != 0).toArray();
 
                 // Instancia o objeto Macaquinho e insere na lista duplamente encadeada
-                listaDeMacaquinhos.append(new Macaquinho(numero, par, impar, coquinhosTotal, coquinhosPares, coquinhosImpares));
+                listaDeMacaquinhos.append(new Macaquinho(numero, enviarPar, enviarImpar, coquinhosTotal, coquinhosPares, coquinhosImpares));
             } else {
-                in.nextLine();
+                // Grava o número de rodadas
+                rodadas = Integer.parseInt(in.nextLine().split(" ")[1]);
             }
-
             idx++;
         }
 
-
-        for (int i = 0; i < 1; i++) {
-            int[] parETotal = new int[2];
-            int[] imparETotal = new int[2];
-            int vencedor = -1;
-            int qtdTotal = -1;
-
+        // Computa toda a lógica do jogo, enviando os coquinhos de um macaquinho para outros
+        int[] parETotal = new int[2];
+        int[] imparETotal = new int[2];
+        int vencedor = -1;
+        int qtdTotal = 0;
+        for (int i = 0; i < rodadas; i++) {
             for (int j = 0; j < listaDeMacaquinhos.size(); j++) {
                 // Macaquinho atual
                 Macaquinho atual = listaDeMacaquinhos.getNode(j).getMacaquinho();
 
                 // Macaquinho que vai receber os coquinhos com pedrinhas PARES
-                listaDeMacaquinhos.getNode(atual.getPar()).getMacaquinho().setCoquinhosPares(atual.getCoquinhosPares());
+                // encontra o nodo do macaquinho -> obtem o objeto macaquinho -> modifica a qtd de coquinhos
+                listaDeMacaquinhos.getNode(atual.getEnviarPar()).getMacaquinho().setCoquinhosPares(atual.getCoquinhosPares());
                 // Registra o total de coquinhos do macaquinho que recebeu a quantidade PAR
-                parETotal[0] = atual.getPar();
-                parETotal[1] = listaDeMacaquinhos.getNode(atual.getPar()).getMacaquinho().getCoquinhosTotal();
-
-                //System.out.println(atual.getPar() + "\n" + Arrays.toString(auxPar));
+                parETotal[0] = atual.getEnviarPar();
+                parETotal[1] = listaDeMacaquinhos.getNode(atual.getEnviarPar()).getMacaquinho().getCoquinhosTotal();
 
                 // Macaquinho que vai receber os coquinhos com pedrinhas IMPARES
-                listaDeMacaquinhos.getNode(atual.getImpar()).getMacaquinho().setCoquinhosImpares(atual.getCoquinhosImpares());
+                // encontra o nodo do macaquinho -> obtem o objeto macaquinho -> modifica a qtd de coquinhos
+                listaDeMacaquinhos.getNode(atual.getEnviarImpar()).getMacaquinho().setCoquinhosImpares(atual.getCoquinhosImpares());
                 // Registra o total de coquinhos do macaquinho que recebeu a quantidade IMPAR
-                imparETotal[0] = atual.getImpar();
-                imparETotal[1] = listaDeMacaquinhos.getNode(atual.getImpar()).getMacaquinho().getCoquinhosTotal();
+                imparETotal[0] = atual.getEnviarImpar();
+                imparETotal[1] = listaDeMacaquinhos.getNode(atual.getEnviarImpar()).getMacaquinho().getCoquinhosTotal();
 
                 // Calculando qual macaquinho tem mais coquinhos
                 if (parETotal[1] > imparETotal[1]) {
                     vencedor = parETotal[0];
                     qtdTotal = parETotal[1];
-                } else {
+                } else if (parETotal[1] < imparETotal[1]){
                     vencedor = imparETotal[0];
                     qtdTotal = imparETotal[1];
                 }
+                System.out.println("\nparaPar: " + parETotal[0] + "\n     qtdTotal: " + parETotal[1]);
+                System.out.println("paraImpar: " + imparETotal[0] + "\n     qtdTotal: " + imparETotal[1]);
 
-                System.out.println();
             }
-
-            System.out.println();
-            System.out.println("Vencedor: " + vencedor + "\nqtdTotal: " + qtdTotal);
-            System.out.println(Arrays.toString(listaDeMacaquinhos.getNode(vencedor).getMacaquinho().getCoquinhosPares()));
-            System.out.println(Arrays.toString(listaDeMacaquinhos.getNode(vencedor).getMacaquinho().getCoquinhosImpares()));
             System.out.println();
         }
+        System.out.println("\n***************************");
+        System.out.println("RODADAS = " + rodadas);
+        System.out.println("\nVencedor: " + vencedor + "\nqtdTotal: " + qtdTotal);
 
 
     }
